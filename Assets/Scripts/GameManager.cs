1148a142bcc2baa5ecdebad01f72246245c04ebd
jhj603 <82034869+jhj603@users.noreply.github.com>
2024-04-17 20:42:48 +0900

    [feat]Add Functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,15 +17,41 @@ public class GameManager : MonoBehaviour
     public GameObject FourthTxt;
     public GameObject NotMatchTxt;
 
+    public GameObject WarningTxt;
+    public GameObject WarningTimeTxt;
+
     public Text timeTxt;
     public GameObject endTxt;
 
     AudioSource audioSource;
+    public AudioClip matchClip;
+    public AudioClip failClip;
+    public AudioClip finishClip;
 
     public int cardCount = 0;
     float timeStack = 0f;
 
+    //프로퍼티 적용해야 함
     public int clickCount = 0;
+
+    float firstTime = 0f;
+    bool isFirst = false;
+
+    public void SetFirst()
+    { 
+        isFirst = true; 
+        WarningTxt.SetActive(true); 
+        WarningTimeTxt.SetActive(true);
+    }
+
+    void ResetFirst()
+    {
+        WarningTxt.SetActive(false);
+        WarningTimeTxt.SetActive(false);
+
+        firstTime = 0f;
+        isFirst = false;
+    }
 
     private void Awake()
     {
@@ -39,6 +65,11 @@ public class GameManager : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
 
         Time.timeScale = 1f;
+
+        clickCount = 0;
+        timeStack = 0f;
+        firstTime = 0f;
+        isFirst = false;
     }
 
     // Update is called once per frame
@@ -47,12 +78,36 @@ public class GameManager : MonoBehaviour
         timeStack += Time.deltaTime;
 
         timeTxt.text = timeStack.ToString("N2");
+
+        if (isFirst)
+        {
+            firstTime += Time.deltaTime;
+
+            WarningTimeTxt.GetComponent<Text>().text = firstTime.ToString("N2");
+
+            if (4f < firstTime)
+            {
+                Invoke("ResetFirst", 1f);
+
+                if (null != firstCard)
+                {
+                    firstCard.CloseCard();
+
+                    firstCard = null;
+                }
+            }
+        }
     }
 
     public void Matched()
     {
+        if (isFirst)
+            ResetFirst();
+
         if (firstCard.idx == secondCard.idx)
         {
+            audioSource.PlayOneShot(matchClip);
+
             if (firstCard.idx == 0 || firstCard.idx == 1)
             {
                 FirstTxt.SetActive(true);
@@ -78,14 +133,24 @@ public class GameManager : MonoBehaviour
             secondCard.DestroyCard();
 
             cardCount -= 2;
+
             if (cardCount == 0)
             {
+                AudioManager.Instance.StopBGM();
+
+                audioSource.PlayOneShot(finishClip);
+
                 endTxt.SetActive(true);
                 Time.timeScale = 0.0f;
             }
         }
         else
         {
+            audioSource.PlayOneShot(failClip);
+
+            firstCard.SetCount(clickCount);
+            secondCard.SetCount(clickCount);
+
             NotMatchTxt.SetActive(true);
             Invoke("HideName", 0.5f);
 
@@ -95,6 +160,8 @@ public class GameManager : MonoBehaviour
 
         firstCard = null;
         secondCard = null;
+
+        ++clickCount;
     }
 
     private void HideName()

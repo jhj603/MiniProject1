403a1f9ea475dd7d73a87f117e11956a2aacdfde
jhj603 <82034869+jhj603@users.noreply.github.com>
2024-04-18 14:12:47 +0900

    [feat]StageChoice

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -42,4 +42,15 @@ public class AudioManager : MonoBehaviour
     {
         audioSource.Stop();
     }
+
+    public void PlayOriginal()
+    {
+        if (null != audioSource)
+        {
+            audioSource.Stop();
+
+            audioSource.clip = originalBGMClip;
+            audioSource.Play();
+        }
+    }
 }
